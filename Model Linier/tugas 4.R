5a2fef068619aa464dc669b2a85cfc1c1a9e000b
Rafhaeloktafandhi <kuroyasha1911@gmail.com>
2025-11-07 08:15:22 +0700

    Refactor R script to load data before modeling
    Removed workspace clearing and added data loading.

--- Model Linier/tugas 4.R
+++ Model Linier/tugas 4.R
@@ -1,3 +1,6 @@
+rm(list=ls())
+data1<-read.csv(file="Model Linier/Students_Grading_Dataset.csv", sep = ";")
+
 # Fit model regresi berganda
 model2 <- lm(Total_Score ~Stress_Level..1.10. + Study_Hours_per_Week, data = data1)
 
@@ -6,3 +9,4 @@ summary(model2)
 
 # Estimasi b0, b1, b2, b3
 coefficients(model2)
+
